391f3eef2711a92e69f5a25cc515a47ca67e3fb1
MischkaPCHI <michellejung4002@gmail.com>
2024-02-26 14:43:42 +0100

    first commit

---
+++ homework/index.js
@@ -0,0 +1,45 @@
+const arr = ["Alex", "Bob", "Chris", "David", "Eva", "Frank", "Grace", "Helen", "Ian", "Jack", "Kate", "Leo", "Max", "Nora", "Oscar"];
+
+
+const newArray = (array) => { return array.filter(elem => elem[0].toLowerCase().includes('a'));}
+
+console.log(newArray(arr));
+
+console.log('-----------------------------------------------');
+
+
+
+const nums = [2, 8 ,24, 8, 1, 7, 45];
+const sum = (arrNums) => { return arrNums.reduce((num1, num2) => num1 + num2);};
+
+console.log(sum(nums));
+
+console.log('-----------------------------------------------');
+
+const numsRevers = (array) => { return array.reverse()}
+
+console.log(numsRevers(nums));
+
+console.log('-----------------------------------------------');
+
+
+const objects = {
+    'customer-id-1': {
+       name: 'William',
+       age: 54
+    },
+    'customer-id-2': {
+       name: 'Tоm',
+       age: 17
+    }
+ };
+
+ const arrayWithObjects = (obj) => {
+    return Object.keys(obj).map(i => ({
+        name: obj[i].name,
+        age: obj[i].age,
+        id: i
+    }))
+ }
+
+ console.log(arrayWithObjects(objects));
